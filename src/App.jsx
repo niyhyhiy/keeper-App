f95a7f9b14b9c5b086c8456d13247982103d2508
Your Name <niyhyhiy@gmail.com>
2025-05-22 13:38:10 -0400

    Finish Keeper App part 1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Note from "./components/note";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Note />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
